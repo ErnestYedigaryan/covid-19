d59fc865a5404c60ad4265c45f0cf6c0ac965603
Ernest Yedigaryan <ernestyedigaryan@gmail.com>
2020-05-06 20:53:22 +0400

    Updates

---
+++ precache-manifest.cc7f28d0ddae3304312cdee944632356.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56af52c0c3f882828ae36225afb4b500",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "823cf80ab9a4264e7e9b",
+    "url": "/covid-19/static/css/2.c1f56dd2.chunk.css"
+  },
+  {
+    "revision": "83275c91507e8e26d754",
+    "url": "/covid-19/static/css/main.e2eca23b.chunk.css"
+  },
+  {
+    "revision": "823cf80ab9a4264e7e9b",
+    "url": "/covid-19/static/js/2.fbdac4f2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/covid-19/static/js/2.fbdac4f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83275c91507e8e26d754",
+    "url": "/covid-19/static/js/main.76d46a69.chunk.js"
+  },
+  {
+    "revision": "c59e2f922549c307064d",
+    "url": "/covid-19/static/js/runtime-main.aabceb11.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid-19/static/media/logo.5d5d9eef.svg"
+  }
+]);
